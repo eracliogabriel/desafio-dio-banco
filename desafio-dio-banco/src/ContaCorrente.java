ef6a7f1291a4f6ce7cfa163f297c8b32aa7ee6a7
eracliogabriel <lopes.biel@hotmail.com>
2024-05-21 20:11:56 -0300

    Novas atualizações

--- desafio-dio-banco/src/ContaCorrente.java
+++ desafio-dio-banco/src/ContaCorrente.java
@@ -4,4 +4,14 @@ public class ContaCorrente {
 	public int agencia;
 	public int numero;
 	public double saldo;
+	
+	public void sacar() {
+	
+	}
+	public void depositar() {
+		
+	}
+	public void transferir() {
+		
+	}
 }
--- desafio-dio-banco/src/ContaPoupanca.java
+++ desafio-dio-banco/src/ContaPoupanca.java
@@ -4,4 +4,14 @@ public class ContaPoupanca {
 	public int agencia;
 	public int numero;
 	public double saldo;
+	
+	public void sacar() {
+		
+	}
+	public void depositar() {
+		
+	}
+	public void transferir() {
+		
+	}
 }
