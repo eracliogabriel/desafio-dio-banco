4066f85865313cbede3292e76e74afa5b382d56a
eracliogabriel <lopes.biel@hotmail.com>
2024-05-21 20:29:12 -0300

    Implementando Interface IConta e desenvolvendo logica da Conta

--- desafio-dio-banco/src/ContaPoupanca.java
+++ desafio-dio-banco/src/ContaPoupanca.java
@@ -1,35 +1,5 @@
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta {
 	
 
-	private int agencia;
-	private int numero;
-	private double saldo;
-	
-	public ContaPoupanca(int agencia, int numero) {
-		this.agencia = agencia;
-		this.numero = numero;
-	}
-	
-	public void sacar() {
-		
-	}
-	public void depositar() {
-		
-	}
-	public void transferir() {
-		
-	}
-
-	public int getAgencia() {
-		return agencia;
-	}
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
 }
