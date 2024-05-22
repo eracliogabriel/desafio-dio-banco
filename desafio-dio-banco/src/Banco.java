e38e5cd4aa1bfce0f5a4c40ab76a3cf38b8a6b30
eracliogabriel <lopes.biel@hotmail.com>
2024-05-21 22:24:55 -0300

    Projeto refatorado e pronto.

--- desafio-dio-banco/src/Banco.java
+++ desafio-dio-banco/src/Banco.java
@@ -1,7 +1,9 @@
+import java.util.List;
 
 public class Banco {
 	
 	public String nome;
+	private List<Conta> contas;
 
 	public String getNome() {
 		return nome;
@@ -10,4 +12,14 @@ public class Banco {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(List<Conta> contas) {
+		this.contas = contas;
+	}
+	
+	
 }
---
+++ desafio-dio-banco/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Eraclio");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		
+		poupanca.imprimirExtrato();
+	}
+
+}
